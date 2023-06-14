200ba48c70157d365d5ffd2bde785d0b62f01f58
Harlocchi <gustaalves1701@gmail.com>
2023-06-14 17:35:57 -0300

    classe Position criada

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,10 +1,12 @@
 package aplication;
 
+import BoardGame.Position;
+
 public class Program {
 
     public static void main(String[] args) {
-
-        System.out.println("heloooooo");
+        Position pos = new Position(2, 3);
+        System.out.println(pos);
 
     }
 
