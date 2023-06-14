c7eb56270ab4b006573b7db7ed90ef8c156a194b
Harlocchi <gustaalves1701@gmail.com>
2023-06-14 18:33:20 -0300

    implementando as classes Board e Pieces

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,12 +1,13 @@
 package aplication;
 
+import BoardGame.Board;
 import BoardGame.Position;
 
 public class Program {
 
     public static void main(String[] args) {
-        Position pos = new Position(2, 3);
-        System.out.println(pos);
+
+        Board board = new Board(8, 8);
 
     }
 
